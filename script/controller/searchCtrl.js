b7badd420453dbe0939286a6fe34e607cc571bfe
newb-pjr <359294117@qq.com>
2017-03-08 17:26:13 +0800

    firstDemo

--- script/controller/searchCtrl.js
+++ script/controller/searchCtrl.js
@@ -3,6 +3,7 @@ angular.module('app').controller('searchCtrl',['dict', '$http', '$scope', functi
 	$scope.search = function(){
 		$http.get('data/positionList.json?name='+$scope.name).then(function(resp){
 			$scope.positionList = resp;
+			console.log(resp)
 		})
 	}
 	$scope.search();
@@ -17,11 +18,38 @@ angular.module('app').controller('searchCtrl',['dict', '$http', '$scope', functi
 		id: 'scale',
 		name: '公司规模'
 	}];
+	var tabId = '';
 	$scope.tClick = function(id,name){
+		tabId = id;
 		$scope.sheet.list = dict[id].data;
 		$scope.sheet.visible = true;
 	}
 	$scope.sClick = function(id,name){
-		console.log(id,name)
+		if(id){
+			angular.forEach($scope.tabList, function(item){
+				console.log(item)
+				if(item.id === tabId){
+					item.name = name;
+				}
+			})
+		}else{
+			angular.forEach($scope.tabList, function(item){
+				if(item.id === tabId){
+					switch (item.id){
+						case 'city':
+							item.name = '城市';
+							break;
+						case 'salary':
+							item.name = '薪水';
+							break;
+						case 'scale':
+							item.name = '公司规模';
+							break;
+						default:
+							break;
+					}
+				}
+			})
+		}
 	}
 }]);
