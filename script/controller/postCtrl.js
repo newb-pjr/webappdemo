460b538f85dfdc2d1b8666a60aa27e8481cb5185
newb-pjr <359294117@qq.com>
2017-03-11 11:52:38 +0800

    201703111152

--- script/controller/postCtrl.js
+++ script/controller/postCtrl.js
@@ -1,2 +1,12 @@
-angular.module('app').controller('postCtrl',['$http','$scope',function($http,$scope){
+angular.module('app').controller('postCtrl',['$scope',function($scope){
+	$scope.tabList = [{
+		id: 'all',
+		name: '全部'
+	},{
+		id: 'pass',
+		name: '面试邀请'
+	},{
+		id: 'fail',
+		name: '不适合'
+	}];
 }]);
