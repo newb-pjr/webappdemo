bf7bc3761532ae529e1349a438f549f5f6c395b7
newb-pjr <359294117@qq.com>
2017-03-17 01:44:36 +0800

    201703170145

--- script/controller/registerCtrl.js
+++ script/controller/registerCtrl.js
@@ -1,5 +1,23 @@
-angular.module('app').controller('registerCtrl',['$http','$scope',function($http,$scope){
+angular.module('app').controller('registerCtrl',['$http','$scope','$interval',function($http,$scope,$interval){
 	$scope.submit = function(){
 		console.log($scope.user)
+	}
+	var count = 60
+	$scope.send = function(){
+		$http.get('data/code.json').then(function(resp){
+			if(resp.data.state == 1){
+				$scope.time = '60s';
+				count = 60;
+				var interval = $interval(function(){
+					count--;
+					if(count<=0){
+						$interval.cancel(interval);
+						$scope.time = '';
+					}else{
+						$scope.time = count + 's';
+					}
+				},1000)
+			}
+		})
 	}
 }]);
