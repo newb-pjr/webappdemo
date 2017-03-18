d900f0abff9ddc4bf6c3bbba5ca6734d62c30f70
newb-pjr <359294117@qq.com>
2017-03-18 12:02:27 +0800

    201703181202

--- script/controller/my.js
+++ script/controller/my.js
@@ -1,2 +1,12 @@
-angular.module('app').controller('myCtrl',['$http','$scope',function($http,$scope){
+angular.module('app').controller('myCtrl',['$http','$scope','$cookieStore','$state',function($http,$scope,$cookieStore,$state){
+	if($cookieStore.get('name')){
+		$scope.image = $cookieStore.get('image');
+		$scope.name = $cookieStore.get('name');
+	}
+	$scope.logout = function(){
+		$cookieStore.remove('id');
+		$cookieStore.remove('name');
+		$cookieStore.remove('image');
+		$state.go('mian');
+	}
 }]);
