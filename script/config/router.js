6b89e3e007251c905f289eb8ab25f55d74c7dcc5
newb-pjr <359294117@qq.com>
2017-05-11 18:26:15 +0800

    201705111826

--- script/config/router.js
+++ script/config/router.js
@@ -1,4 +1,17 @@
-angular.module('app').config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+angular.module('app').config(['$stateProvider','$urlRouterProvider','$ionicConfigProvider',function($stateProvider,$urlRouterProvider,$ionicConfigProvider) {
+	$ionicConfigProvider.platform.ios.tabs.style('standard'); 
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('bottom');
+
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
+    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');        
+
+    $ionicConfigProvider.platform.ios.views.transition('ios'); 
+    $ionicConfigProvider.platform.android.views.transition('android');
 	$stateProvider.state('tabs',{
 		url: '/tabs',
 		abstract:true,
