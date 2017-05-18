d77d34aa031c555dfc6de50a60c09029efaa3a7b
newb-pjr <359294117@qq.com>
2017-05-19 01:21:17 +0800

    201705190121

--- script/controller/favoriteCtrl.js
+++ script/controller/favoriteCtrl.js
@@ -1,4 +1,4 @@
-angular.module("app").controller('favoriteCtrl', ['$scope','$http' function($scope,$http){
+angular.module("app").controller('favoriteCtrl', ['$scope','$http', function($scope,$http){
 	$http.get("data/myFavorite.json").success(function(resp){
 		$scope.favoriteList = resp;
 	})
--- script/directive/positonList.js
+++ script/directive/positonList.js
@@ -1,7 +1,22 @@
-angular.module("app").directive('appPosition', [function(){
+angular.module("app").directive('appPosition', ['$http',function($http){
 	return {
 		restrict: 'EA',
 		replace: true,
-		templateUrl: 'view/template/tpl/positionList.html'
+		templateUrl: 'view/template/tpl/positionList.html',
+		scope: {
+			data: '=',
+			isFavorite: '='
+		},
+		link: function(scope){
+			scope.select = function(item,$event){
+				$http.post("data/favorite.json",{
+					id: item.id,
+					select: !item.select
+				}).success(function(resp){
+					item.select = !item.select;
+				})
+				 // $event.stopPropagation(); 
+			}
+		}
 	};
 }]);
