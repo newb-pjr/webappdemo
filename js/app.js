7ffce818f62074c1ea41c77dfea1bd074caea541
newb-pjr <359294117@qq.com>
2017-06-09 01:16:46 +0800

    201706090116

---
+++ js/app.js
@@ -0,0 +1,54 @@
+angular.module("app",["ionic"]).config(['$stateProvider','$urlRouterProvider','$ionicConfigProvider',function($stateProvider,$urlRouterProvider,$ionicConfigProvider) {
+	$ionicConfigProvider.platform.ios.tabs.style('standard');
+	$ionicConfigProvider.platform.ios.tabs.position('bottom');
+	$ionicConfigProvider.platform.android.tabs.style('standard');
+	$ionicConfigProvider.platform.android.tabs.position('standard');
+
+	$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+	$ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+	$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+	$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+	$ionicConfigProvider.platform.ios.views.transition('ios');
+	$ionicConfigProvider.platform.android.views.transition('android');
+
+	$stateProvider
+		.state('tabs',{
+			url: '/tabs',
+			templateUrl: 'view/tabs.html'
+		})
+		.state('tabs.home',{
+			url: '/home',
+			views: {
+				'tab-home': {
+					templateUrl: 'view/tabs/home.html'
+				}
+			}
+		})
+		.state('tabs.news',{
+			url: '/news',
+			views: {
+				'tab-news': {
+					templateUrl: 'view/tabs/news.html'
+				}
+			}
+		})
+		.state('tabs.thread',{
+			url: '/thread',
+			views: {
+				'tab-thread': {
+					templateUrl: 'view/tabs/thread.html'
+				}
+			}
+		})
+		.state('tabs.user',{
+			url: '/user',
+			views: {
+				'tab-user': {
+					templateUrl: 'view/tabs/user.html'
+				}
+			}
+		});
+	$urlRouterProvider.otherwise('/tabs/home');
+}])
