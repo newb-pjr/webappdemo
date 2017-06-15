170f6103c99c727b00b2f7c4e589dd18ee67d1b4
newb-pjr <359294117@qq.com>
2017-06-16 03:55:13 +0800

    201706160355

---
+++ js/app.js
@@ -0,0 +1,30 @@
+angular.module("app",['ionic']).config(['$ionicConfigProvider','$stateProvider','$urlRouterProvider',function($ionicConfigProvider,$stateProvider,$urlRouterProvider) {
+	$ionicConfigProvider.platform.ios.tabs.style('standard');
+	$ionicConfigProvider.platform.ios.tabs.position('bottom');
+	$ionicConfigProvider.platform.android.tabs.style('standard');
+	$ionicConfigProvider.platform.android.tabs.position('standard');
+
+	$ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+	$ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+	$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+	$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+	$ionicConfigProvider.platform.ios.views.transition('ios');
+	$ionicConfigProvider.platform.android.views.transition('android');
+
+	$stateProvider
+		.state('tabs',{
+			url: '/tabs',
+			templateUrl: 'view/tabs.html'
+		})
+		.state('tabs.home',{
+			url: '/home',
+			views: {
+				'tab-home': {
+					templateUrl: 'view/tabs/home.html'
+				}
+			}
+		})
+	$urlRouterProvider.otherwise('tabs/home')
+}])
