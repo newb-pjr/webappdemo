97e98ac1ee407f8d29e24bfe9caf075ede5486fe
newb-pjr <359294117@qq.com>
2017-10-30 02:09:02 +0800

    201710300208

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>helloworld</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/variable.styl'
+
+ #app {
+  color: $color-theme
+ }
+</style>
