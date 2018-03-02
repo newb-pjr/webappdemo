26a9c73d9312b4cfc30ec8a862a844914d4871f9
pang <359294117@qq.com>
2018-03-02 16:29:14 +0800

    201803021630

--- src/components/head/head.vue
+++ src/components/head/head.vue
@@ -8,9 +8,13 @@
 			</div>
 			<div class="description">{{seller.description}} / {{seller.deliveryTime}}分钟送达</div>
 			<div class="supports">
-				<div class="type" :class=""></div>
-				<span class="desc">{{seller.supports[0].description}}</span>
+				<div class="type" :class="typePic"></div>
+				<span class="desc" v-if="seller.supports">{{seller.supports[0].description}}</span>
 			</div>
+		</div>
+		<div class="supports-number">
+			<span class="number">{{seller.supports.length}}个</span>
+			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 	</div>
 </template>
@@ -23,13 +27,39 @@ export default {
 				return {}
 			}
 		}
+	},
+	computed: {
+		typePic () {
+			let cls
+			if (!this.seller.supports) {
+				return
+			}
+			switch (this.seller.supports[0].type) {
+				case 0:
+					cls = 'decrease'
+					break
+				case 1:
+					cls = 'discount'
+					break
+				case 2:
+					cls = 'special'
+					break
+				case 3:
+					cls = 'invoice'
+					break
+				case 4:
+					cls = 'guarantee'
+					break
+			}
+			return cls
+		}
 	}
 }
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
-	@import	'~common/stylus/mixin'
-	@import	'~common/stylus/variable'
+	@import	'~common/stylus/index'
 	.header-container
+		position: relative
 		padding-top: 24px
 		padding-left: 24px
 		background-color: #000
@@ -37,11 +67,12 @@ export default {
 		.avatar
 			display: inline-block
 			vertical-align: top
+			border-radius: 3px
 		.info-container
 			display: inline-block
 			margin-left: 16px
 			.title
-				margin-bottom: 2px
+				margin-bottom: 5px
 				margin-top: 2px
 				font-size: 0
 				color: $color-text
@@ -60,6 +91,7 @@ export default {
 					margin-left: 6px
 					font-size: 16px
 			.description
+				margin-bottom: 8px
 				font-size: 12px
 				color: $color-text
 				font-weight: 200
@@ -67,8 +99,11 @@ export default {
 			.supports
 				font-size: 0
 				.type
+					display: inline-block
+					margin-right: 4px
 					width: 12px
 					height: 12px
+					vertical-align: top
 					background-size: 12px 12px
 					background-repeat: no-repeat
 					&.decrease
@@ -82,8 +117,22 @@ export default {
 					&.special
 						bg-image('special_1')
 				.desc
+					display: inline-block
 					font-size: 10px
 					color: $color-text
 					font-weight: 200
 					line-height: 12px
+		.supports-number
+			position: absolute
+			right: 12px
+			bottom: 46px
+			padding: 7px 8px
+			background-color: rgba(0, 0, 0, 0.2)
+			border-radius: 24px
+			font-size: 10px
+			color: $color-text
+			font-weight: 200
+			line-height: 12px
+			.number
+				margin-right: 2px
 </style>
