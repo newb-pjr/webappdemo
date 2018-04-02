3c50057920180d5b3584816e4c683d371593cbce
pang <359294117@qq.com>
2018-04-02 18:07:04 +0800

    201804021807

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -2,4 +2,22 @@ export function formatDate (date, format) {
 	if (/(y+)/.test(format)) {
 		format = format.replace(RegExp.$1, (date.getFullYear() + '').subStr(4 - RegExp.$1.length))
 	}
+
+	let o = {
+		'M+': date.getMonth() + 1,
+		'd+': date.getDate(),
+		'H+': date.getHours(),
+		'm+': date.getMinutes()
+	}
+
+	for (let k in o) {
+		if (new RegExp(`(${k})`).test(format)) {
+			let str = o[k] + ''
+			format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? str : padleftZore(str)
+		}
+	}
+}
+
+function padleftZore (str) {
+	return '00' + str.subStr(str.length)
 }
